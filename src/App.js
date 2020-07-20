cbf3afc42dc46a90168a7d1a4d241b91dd3600e6
RNA <r.nair@castsoftware.com>
2020-07-21 00:59:07 +0530

    add search bar component use deBouncing with react hooks

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './components/search-bar/search-bar.component.jsx';
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="app" id="app">
+            <SearchBar/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/search-bar/search-bar.component.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import './search-bar.styles.scss';
+
+const SearchBar = () => {
+
+    const [value, setValue] = useState('');
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleClick = () => {
+        setValue('');
+    }
+
+    const search = () => {
+        setValue('');
+    }
+
+    useEffect(() => {
+        if(value !== '') {
+            const timeOut = setTimeout(search, 500);
+
+            return () => {
+                clearTimeout(timeOut);
+            }
+        }
+    }, [value]);
+
+    const handleIcon = () => {
+        document.getElementById('cancel-button').classList.toggle('focus')
+    }
+
+    return (
+        <div className="search-container">
+            <div className="search-bar">
+                    <input
+                        id = 'search'
+                        name = 'search'
+                        type = 'search'
+                        placeholder = 'Type in a git username....'
+                        onFocus = { handleIcon }
+                        onBlur = { handleIcon }
+                        onChange = { handleChange }
+                        value = { value }
+                        autoComplete = 'off'
+                        autoFocus
+                    >
+                    </input>
+                <div className="cancel-button" id='cancel-button' onClick = {handleClick}>
+                    <i className="fa fa-trash"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
